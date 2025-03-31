198f9e80ed472c0232177b696258212dc68f3c9d
Praityush Prakash <pratiyush@ti.com>
2025-03-31 12:23:39 +0530

    refactor: separate app and scheduler python file
    Signed-off-by: Praityush Prakash <pratiyush@ti.com>

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import datetime
 import os
+import datetime
+import smtplib
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from apscheduler.schedulers.background import BackgroundScheduler
-import smtplib
 from email.mime.text import MIMEText
 
 # Define the Flask application
-# Setting up SQLIte database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reminders.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Define the Reminder database model
@@ -27,93 +26,69 @@ class Reminder(db.Model):
         self.date_time = date_time
         self.type = type
         self.frequency = frequency
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'date_time': self.date_time.isoformat(),
+            'type': self.type,
+            'frequency': self.frequency
+        }
 
 # Function to send email
 def send_email(subject, body):
-    # Get email credentials from environment variable
     email_username = os.environ.get('EMAIL_USERNAME')
     email_password = os.environ.get('EMAIL_PASSWORD')
     email_to_address = os.environ.get('EMAIL_TO_ADDRESS')
     email_from_address = os.environ.get('EMAIL_FROM_ADDRESS')
 
-
-    # Create email message
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['To'] = email_to_address
     msg['From'] = email_from_address
 
-    # Send email
     try:
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
             server.login(email_username, email_password)
             server.sendmail(email_from_address, email_to_address, msg.as_string())
         print("Email sent to {}".format(email_to_address))
     except Exception as e:
-        print("Error sending email to {} : {}".format(email_to_address, e))
-
-# Function to check for today's reminders
-def check_reminders():
-    today = datetime.datetime.now().date()
-
-    reminders = Reminder.query.filter(db.func.date(Reminder.date_time) == today).all()
-    for reminder in reminders:
-        subject = f"Reminder: {reminder.type} for {reminder.title}"
-        body = f"Hey, don't forget! Today is \nEvent Type: {reminder.type}:\nTitle: {reminder.title}\nDescription: {reminder.description}"
-        send_email(subject, body)
-
-        # Based on event frequency 
-        match reminder.frequency:
-            case "daily":
-                reminder.date_time += datetime.timedelta(days=1)
-            case "weekly":
-                reminder.date_time += datetime.timedelta(weeks=1)
-            case "monthly":
-                reminder.date_time += datetime.timedelta(months=1)
-            case "yearly":
-                reminder.date_time += datetime.timedelta(years=1)
-            case "once":
-                db.session.delete(reminder)
-                db.session.commit()
-            case _:
-                pass
-
-# Schedule daily job
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=check_reminders, trigger="interval", days=1)
-scheduler.start()
+        print("Error sending email: {}".format(e))
 
 # API endpoint to add new reminders
 @app.route('/reminder', methods=['POST'])
 def add_reminder():
-    title = request.json['title']
-    description = request.json['description']
-    date_time = datetime.datetime.strptime(request.json['date_time'], "%Y-%m-%dT%H:%M:%S.%fZ")
-    type = request.json['type']
-    frequency = request.json['frequency']
+    data = request.get_json()
+    try:
+        title = data['title']
+        description = data['description']
+        date_time = datetime.datetime.strptime(data['date_time'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        type = data['type']
+        frequency = data['frequency']
 
-    new_reminder = Reminder(title, description, date_time, type, frequency)
-    db.session.add(new_reminder)
-    db.session.commit()
+        new_reminder = Reminder(title, description, date_time, type, frequency)
+        db.session.add(new_reminder)
+        db.session.commit()
 
-    return jsonify({'message': 'Reminder added successfully'}), 200
+        return jsonify({'message': 'Reminder added successfully'}), 200
+    except Exception as e:
+        return jsonify({'message': 'Error adding reminder: {}'.format(e)}), 400
 
 # API endpoint to get all reminders
 @app.route('/reminders', methods=['GET'])
 def get_reminders():
     reminders = Reminder.query.all()
-    return jsonify([reminder.to_dict() for reminder in reminders]), 200
+    return jsonify([r.to_dict() for r in reminders]), 200
 
+# API to test email sending
 @app.route('/send/email/test', methods=['GET'])
 def send_email_test():
-    subject = "Test email"
-    body = "This is a test email"
-    send_email(subject, body)
+    send_email("Test Email", "This is a test email.")
     return jsonify({'message': 'Email sent successfully'}), 200
-
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=10000)
